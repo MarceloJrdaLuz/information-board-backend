3ac6987661e7951c343743fe53fe39faaf8fcf04
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2024-11-11 11:46:07 -0300

    update

--- dist/controllers/CronJobController/index.js
+++ dist/controllers/CronJobController/index.js
@@ -1,8 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const noticeRepository_1 = require("../../repositories/noticeRepository");
+const typeorm_1 = require("typeorm");
 class CronJobController {
     async deleteNotices(req, res) {
         console.log('Task finished');
+        const notices = await noticeRepository_1.noticeRepository.find({ where: {
+                expired: (0, typeorm_1.LessThanOrEqual)(new Date())
+            } });
+        console.log(notices);
+        res.send().json({ message: "Concluido" });
     }
 }
 exports.default = new CronJobController();
--- src/controllers/CronJobController/index.ts
+++ src/controllers/CronJobController/index.ts
@@ -1,8 +1,18 @@
 import { Response, Request } from "express";
+import { noticeRepository } from "../../repositories/noticeRepository";
+import { LessThanOrEqual } from "typeorm";
 
 class CronJobController {
     async deleteNotices(req: Request, res: Response) {
         console.log('Task finished')
+
+        const notices = await noticeRepository.find({where: {
+            expired: LessThanOrEqual(new Date())
+        }})
+
+        console.log(notices)
+
+        res.send().json({message: "Concluido"})
     }
 }
 
