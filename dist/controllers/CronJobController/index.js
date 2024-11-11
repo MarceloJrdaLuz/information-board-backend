24983a6d29e69415d4d0e466ba6fa5fb7aaa5c14
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2024-11-11 12:43:46 -0300

    updated

--- dist/controllers/CronJobController/index.js
+++ dist/controllers/CronJobController/index.js
@@ -2,14 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const noticeRepository_1 = require("../../repositories/noticeRepository");
 const typeorm_1 = require("typeorm");
+const api_errors_1 = require("../../helpers/api-errors");
 class CronJobController {
     async deleteExpiredNotices(req, res) {
-        console.log('Task finished');
-        const notices = await noticeRepository_1.noticeRepository.find({ where: {
-                expired: (0, typeorm_1.LessThanOrEqual)(new Date())
-            } });
-        console.log(notices);
-        res.send().json({ message: "Concluido" });
+        const expiredNotices = await noticeRepository_1.noticeRepository.find({
+            where: {
+                expired: (0, typeorm_1.LessThan)(new Date())
+            }
+        });
+        if (expiredNotices.length === 0) {
+            throw new api_errors_1.NotFoundError("No expired notices found");
+        }
+        try {
+            await noticeRepository_1.noticeRepository.remove(expiredNotices);
+            return res.status(200).json({ message: "Expired notices deleted", notices: expiredNotices });
+        }
+        catch (error) {
+            console.log(error);
+            throw new Error("Error deleting expired notices");
+        }
     }
 }
 exports.default = new CronJobController();
--- src/controllers/CronJobController/index.ts
+++ src/controllers/CronJobController/index.ts
@@ -1,18 +1,28 @@
-import { Response, Request } from "express";
-import { noticeRepository } from "../../repositories/noticeRepository";
-import { LessThanOrEqual } from "typeorm";
+import { Response, Request } from "express"
+import { noticeRepository } from "../../repositories/noticeRepository"
+import { LessThanOrEqual, LessThan } from "typeorm"
+import { NotFoundError } from "../../helpers/api-errors"
 
 class CronJobController {
     async deleteExpiredNotices(req: Request, res: Response) {
-        console.log('Task finished')
+        const expiredNotices = await noticeRepository.find({
+            where: {
+                expired: LessThan(new Date())
+            }
+        })
 
-        const notices = await noticeRepository.find({where: {
-            expired: LessThanOrEqual(new Date())
-        }})
+        if (expiredNotices.length === 0) {
+            throw new NotFoundError("No expired notices found")
+        }
 
-        console.log(notices)
+        try {
+            await noticeRepository.remove(expiredNotices)
+            return res.status(200).json({ message: "Expired notices deleted", notices: expiredNotices })
+        } catch (error) {
+            console.log(error)
+            throw new Error("Error deleting expired notices")
+        }
 
-        res.send().json({message: "Concluido"})
     }
 }
 
