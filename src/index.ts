779be79365a166c2dab7ae58daa5e4a2588dc7bd
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2025-11-06 15:49:53 -0300

    remove console

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,6 @@ AppDataSource.initialize().then(() => {
     const app = express()
     app.use(express.json())
     app.use(cors(corsOptions))
-    console.log(corsOptions)
     app.use(cookieParser())
     app.use(routes)
 
