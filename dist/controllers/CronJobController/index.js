cc76ef5950f8ccbbde20469895b20903ce3128b0
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2025-12-09 13:12:31 -0300

    update

--- dist/controllers/CronJobController/index.js
+++ dist/controllers/CronJobController/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const noticeRepository_1 = require("../../repositories/noticeRepository");
 const typeorm_1 = require("typeorm");
 const api_errors_1 = require("../../helpers/api-errors");
 const moment_timezone_1 = __importDefault(require("moment-timezone"));
@@ -13,12 +14,10 @@ const meetingAssistanceRepository_1 = require("../../repositories/meetingAssista
 const mailer_1 = __importDefault(require("../../modules/mailer"));
 const child_process_1 = require("child_process");
 const config_1 = require("../../config");
-
 class CronJobController {
     async deleteExpiredNotices(req, res) {
-        
         const startOfToday = (0, moment_timezone_1.default)().startOf('day').toDate();
-        const expiredNotices = await noticeRepository.find({
+        const expiredNotices = await noticeRepository_1.noticeRepository.find({
             where: {
                 expired: (0, typeorm_1.LessThan)(startOfToday)
             }
@@ -27,7 +26,7 @@ class CronJobController {
             throw new api_errors_1.NotFoundError("No expired notices found");
         }
         try {
-            await noticeRepository.remove(expiredNotices);
+            await noticeRepository_1.noticeRepository.remove(expiredNotices);
             return res.status(200).json({ message: "Expired notices deleted", notices: expiredNotices });
         }
         catch (error) {
