1af6f6d2a6cba4533584eb809d53576644d6bb47
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2025-12-09 08:50:52 -0300

    adicionado na pasta dist também

--- dist/middlewares/gitHubCronAuth.js
+++ dist/middlewares/gitHubCronAuth.js
@@ -4,8 +4,12 @@ exports.verifyGitHubCron = void 0;
 const api_errors_1 = require("../helpers/api-errors");
 const config_1 = require("../config");
 function verifyGitHubCron(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (authHeader !== `Bearer ${config_1.config.cron_secret_github}`) {
+    var _a;
+    const authHeader = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.trim();
+    const expectedSecret = `Bearer ${config_1.config.cron_secret_github}`.trim();
+    console.log("AuthHeader received from request:", JSON.stringify(authHeader));
+    console.log("Expected GitHub secret:", JSON.stringify(expectedSecret));
+    if (!authHeader || authHeader !== expectedSecret) {
         throw new api_errors_1.UnauthorizedError('GitHub Cron secret invalid');
     }
     return next();
