5266bc6f0df558a04bbe3310b45c28a326b38bb6
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2026-08-31 00:01:26 -0300

    fix: replace diskStorage with memoryStorage in uploadXml to fix EROFS on Vercel

--- dist/config/multer.js
+++ dist/config/multer.js
@@ -47,12 +47,7 @@ exports.uploadFile = (0, multer_1.default)({
     }
 });
 exports.uploadXml = (0, multer_1.default)({
-    storage: multer_1.default.diskStorage({
-        destination: path_1.default.resolve(__dirname, "..", "..", "tmp", "uploads"),
-        filename: (req, file, cb) => {
-            cb(null, `${(0, uuid_1.v4)()}-${file.originalname.trim()}`);
-        }
-    }),
+    storage: multer_1.default.memoryStorage(),
     limits: {
         fileSize: 20 * 1024 * 1024
     }
