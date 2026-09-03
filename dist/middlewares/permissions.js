5579fd094f7996e9a3c61545ea20e398c0ad3ea5
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2026-09-03 00:28:50 -0300

    Revert "feat: enhance permissions middleware and public witness scheduling logic with new assignment rules"
    This reverts commit 946fa5e543bc1378176046b41c7c8befefc6c1a5.

--- dist/middlewares/permissions.js
+++ dist/middlewares/permissions.js
@@ -1,37 +1,15 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.is = exports.requirePublisher = exports.verifyCronSecret = exports.decoder = void 0;
-const jsonwebtoken_1 = __importStar(require("jsonwebtoken"));
-const process_1 = __importDefault(require("process"));
-const config_1 = require("../config");
+const jsonwebtoken_1 = require("jsonwebtoken");
 const api_errors_1 = require("../helpers/api-errors");
 const userRepository_1 = require("../repositories/userRepository");
+const jsonwebtoken_2 = __importDefault(require("jsonwebtoken"));
+const process_1 = __importDefault(require("process"));
+const config_1 = require("../config");
 async function decoder(request) {
     var _a, _b, _c, _d;
     const authHeader = request.headers.authorization;
@@ -47,7 +25,7 @@ async function decoder(request) {
         throw new api_errors_1.UnauthorizedError('Token malformatted');
     }
     const jwtPass = (_a = process_1.default.env.JWT_PASS) !== null && _a !== void 0 ? _a : "";
-    jsonwebtoken_1.default.verify(token, jwtPass, (err, decoded) => {
+    jsonwebtoken_2.default.verify(token, jwtPass, (err, decoded) => {
         if (err) {
             throw new api_errors_1.UnauthorizedError('Token invalid');
         }
@@ -87,26 +65,29 @@ function requirePublisher() {
 exports.requirePublisher = requirePublisher;
 function is(role) {
     const roleAuthorized = async (req, res, next) => {
-        var _a;
+        var _a, _b, _c, _d;
         const user = await decoder(req);
-        // pega do body ou dos params
-        const congregation_id = req.body.congregation_id || req.params.id;
-        const userRoles = (_a = user === null || user === void 0 ? void 0 : user.roles) === null || _a === void 0 ? void 0 : _a.map(role => role.name);
+        // pega do body ou dos params se explicitamente for congregação
+        const isCongregationRoute = ((_a = req.baseUrl) === null || _a === void 0 ? void 0 : _a.includes('congregation')) || ((_b = req.path) === null || _b === void 0 ? void 0 : _b.includes('/congregation/')) || ((_c = req.path) === null || _c === void 0 ? void 0 : _c.includes('/congregations'));
+        const congregation_id = req.body.congregation_id || req.params.congregation_id || req.params.congregationId || (isCongregationRoute ? req.params.id : undefined);
+        const userRoles = (_d = user === null || user === void 0 ? void 0 : user.roles) === null || _d === void 0 ? void 0 : _d.map(role => role.name);
         const rolesExists = userRoles === null || userRoles === void 0 ? void 0 : userRoles.some(r => role.includes(r));
         if (rolesExists) {
             if (userRoles === null || userRoles === void 0 ? void 0 : userRoles.includes("ADMIN")) {
                 return next();
             }
             else {
-                // se for admin_congregation, precisa pertencer à congregação
-                const userCongregation = await userRepository_1.userRepository.find({
-                    where: {
-                        id: user.id,
-                        congregation: { id: congregation_id }
+                // se for admin_congregation ou outro cargo congregacional e um congregation_id foi fornecido, precisa pertencer à congregação
+                if (congregation_id) {
+                    const userCongregation = await userRepository_1.userRepository.find({
+                        where: {
+                            id: user.id,
+                            congregation: { id: congregation_id }
+                        }
+                    });
+                    if (userCongregation.length < 1) {
+                        throw new api_errors_1.UnauthorizedError('User is not admin in this congregation');
                     }
-                });
-                if (userCongregation.length < 1) {
-                    throw new api_errors_1.UnauthorizedError('User is not admin in this congregation');
                 }
                 return next();
             }
